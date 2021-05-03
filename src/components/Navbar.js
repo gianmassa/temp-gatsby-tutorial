e1123d5b8d07d7c6a4ae8edf9637dec64d29b808
Giancarlo Massarani Alves <MassarGi@basfad.basf.net>
2021-05-03 17:58:47 -0300

    Added functionality to navbar Aula 28

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
+import { FiAlignJustify } from 'react-icons/fi'
+import logo from '../assets/images/logo.svg'
 
 const Navbar = (props) => {
+  const [show, setShow] = useState(false)
+
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/tags">Tags</Link></li>
-        <li><Link to="/recipes">Recipes</Link></li>
-      </ul>
+    <nav class="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <Link to="/">
+            <img src={logo} alt="Simply Recipes Logo" />
+          </Link>
+          <button className="nav-btn" onClick={()=>setShow(!show)}>
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div class={show ? "nav-links show-links" : "nav-links"}>
+          <Link to="/" className="nav-link" activeClassName="active-link" onClick={()=>setShow(false)}>Home</Link>
+          <Link to="/recipes" className="nav-link" activeClassName="active-link" onClick={()=>setShow(false)}>Recipes</Link>
+          <Link to="/tags" className="nav-link" activeClassName="active-link" onClick={()=>setShow(false)}>Tags</Link>
+          <Link to="/about" className="nav-link" activeClassName="active-link" onClick={()=>setShow(false)}>About</Link>
+          <div className="nav-link contact-link">
+            <Link to="/contact" className="btn">Contact</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   )
 }
